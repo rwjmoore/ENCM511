b2983663b41648b35751a861c84387514a7d56f0
E-man248 <67338595+E-man248@users.noreply.github.com>
2021-11-02 15:19:27 -0600

    Update IOs.c

--- DriverProject4.X/IOs.c
+++ DriverProject4.X/IOs.c
@@ -85,25 +85,37 @@ uint16_t do_ADC(void)
 
     /* ------------- ADC INITIALIZATION ------------------*/
 
-    AD1CON1bits.ADON = 1; // turn on ADC module
-
-    // ADD CODE
     // Configure ADC by setting bits in AD1CON1 register ? slides 13-14
-    // ADD CODE
-
-
-    AD1CON2bits.VCFG = 0b000; // Selects AVDD, AVSS (supply voltage to PIC) as Vref
+    AD1CON1bits.ADON = 1; // turn on ADC module
+    AD1CON1bits.FORM = 00; // Sets the data output format to int
+    AD1CON1bits.SSRC = 111; // Software triggered ADC Conversion
+    AD1CON1bits.ASAM = 0; // Sampling starts when SAMP Bit set
     
-    // ADD CODE
-    // Configure ADC by setting bits in AD1CON2 as shown in slides 15-16
-    // ADD CODE
+    // Configure ADC by setting bits in AD1CON1 as shown in slides 15-16
+    AD1CON2bits.VCFG = 0b000; // Selects AVDD, AVSS (supply voltage to PIC) as Vref
+    AD1CON2bits.CSCNA = 0; // Prevent input scanning
+    /* * */ AD1CON2bits.SMPI = 0000; // Interrupts  
     
     AD1CON3bits.ADRC = 0; // Use system clock
     
-    // ADD CODE
-    //Configure the ADC?s sample time by setting bits in AD1CON3 shown in slide 17
+    //Configure the ADC?s sample time by setting bits in AD1CON2 shown in slide 17  
+    AD1CON2bits.BUFM = 0; // Buffer is configured as  16 word buffer
+    AD1CON2bits.ALTS = 0; // Make sure to use MUX A input setting
+    
+    //Configure the ADC?s sample time by setting bits in AD1CON3
+    /* * */ AD1CON3bits.ADRC = 0; // System Clock - A/D Conversion Clock Source Bit
+    AD1CON3bits.SAMC = 11111; // 31 TAD - Auto Sample Time Bits
+    //AD1CON3bits.ADCS = 0;  
     // ADD CODE // Ensure sample time is 1/10th of signal being sampled
     // ADD CODE // Select and configure ADC input as shown in slides 18-20
+    
+    AD1CHSbits.CH0NA = 0; // Channel 0 negative input select for MUX A
+    AD1CHSbits.CH0SA = 0101; // Channel 0 positive input select for MUX A
+    
+    AD1PCFGbits.PCFG5 = 0; // Sets pin to Analog Mode
+    AD1CSSL = 0;
+    
+    
         
 /* ------------- ADC SAMPLING AND CONVERSION ------------------*/
 
